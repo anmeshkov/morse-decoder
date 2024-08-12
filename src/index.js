cce53a13c21c4968545eafb00e8886a21b432013
Andrew Meshkov <meshkov89@gmail.com>
2024-08-12 10:36:56 +0300

    feat: compleat task

--- src/index.js
+++ src/index.js
@@ -39,7 +39,36 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+    const arr = [];
+  
+    // разбиваем строку на массив по 10 имволов
+    for (let i = 0; i < expr.length; i += 10) {
+      const chunk = expr.slice(i, i + 10);
+  
+      // если 10 символов это звездочки, то пушим пробел
+      if (chunk === "**********") {
+        arr.push(" ");
+      } else {
+        // иначе разбиваем подстроку из 10 символов на массви по 2 символоа
+        const codeString = [];
+        for (let j = 0; j < chunk.length; j += 2) {
+          codeString.push(chunk.slice(j, j + 2));
+        }
+  
+        // по ключу ищем символ
+        const string = codeString.map(el => {
+          if (el === '00') return ''
+          if (el === '10') return '.'
+          if (el === '11') return '-'
+        }).join('')
+  
+        arr.push(MORSE_TABLE[string]);
+  
+      }
+    }
+    
+    return arr.join('');
+  }
 
 module.exports = {
     decode
